f367bc234d268888100cf8c060ad4d5d1600af6a
Allen Baker <althebaker43@gmail.com>
2012-06-23 11:29:07 -0500

    BaseStation update: Continued formatting of code and addition of doxygen comments.

--- BaseStation/Serial.h
+++ BaseStation/Serial.h
@@ -1,3 +1,14 @@
+/**
+ * \file Serial.h
+ *
+ * \brief Serial communications interface header file.
+ *
+ * \details This header file contains the necessary includes for serial port
+ * use and declares publicly available methods that may be used for communication with
+ * the robot. It also defines some values that are intended to help build
+ * serial communications packets.
+ */
+
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -6,15 +17,27 @@
 #include <sys/stat.h>
 #include <string.h>
 
-#define	PKT_BND	0xFF
-#define VAL_NUL 0x55        // Value filler for hello, standby, goodbye, and ready packets
+#define	PKT_BND	0xFF        /**< Value to fill the front_bnd and end_bnd fields
+                              of packet with */
+#define VAL_NUL 0x55        /**< Value filler for PKT_HELLO, PKT_STDBY,
+                              PKT_GDBY, and PKT_RDY packets */
 
-int init_serial(char *port);
+/**
+ * \brief Serial communication intialization function.
+ */
+int init_serial( char* port );
 
-packet * read_serial();
+/**
+ * \brief Read-from-serial function.
+ */
+packet* read_serial( void );
 
-int write_serial(packet * msg);
+/**
+ * \brief Write-to-serial function.
+ */
+int write_serial( packet* msg );
 
-void close_serial();
-
-void say_hello();
+/**
+ * \brief Serial communication termination function.
+ */
+void close_serial( void );
